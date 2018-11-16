cddb924477081f1862df9204c43ea16c3d16df1e
Koichi Miyamoto <m5813k@gmail.com>
2018-11-17 00:42:16 +0900

    nuxt

---
+++ pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        about
+      </h1>
+      <h2 class="subtitle">
+        My world-class Nuxt.js about
+      </h2>
+      <div class="links">
+        {{ detail }}
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    detail: 'hogehoge hoge'
+  }
+}
+</script>
